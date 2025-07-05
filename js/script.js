b8802ad735d1c885943c83cc81058be6983a5f47
Duván Verdún <dieduver1@gmail.com>
2025-07-04 21:54:20 -0300

    Agregar interactividad con JavaScript: cambio de color y ejercicios basicos

--- js/script.js
+++ js/script.js
@@ -9,3 +9,33 @@ let hobbies = ["Programar", "Entrenar", "Leer"];
 let persona = { nombre: "Duván", edad: 15 };
 
 console.log(nombre, edad, esEstudiante, hobbies, persona);
+
+let añoActual = 2025;
+let añoNacimiento = 2010;
+let calculoEdad = añoActual - añoNacimiento;
+
+if (calculoEdad >= 16) {
+    console.log("Tienes 16 años o más");
+} else {
+    console.log("Tienes menos de 16 años");
+};
+
+// Función normal
+function saludar(nombre) {
+    console.log("Hola, " + nombre + "!");
+};
+
+//Función flecha
+const despedir = (nombre) => {
+    console.log("Chau, " + nombre + "!");
+};
+
+saludar("Duván");
+despedir("Duván");
+
+
+const button = document.getElementById("cambiarColorBtn");
+
+button.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#"+Math.floor(Math.random()*16777215).toString(16);
+});
