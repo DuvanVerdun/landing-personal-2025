e33dff3d07d514bd1a4b5b157f360ff8b9775265
Duván Verdún <dieduver1@gmail.com>
2025-07-04 21:33:48 -0300

    Agregar javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+// Variables
+let nombre = "Duván";
+const edad = 15;
+
+// Tipos de datos
+let esEstudiante = true;
+let hobbies = ["Programar", "Entrenar", "Leer"];
+let persona = { nombre: "Duván", edad: 15 };
+
+console.log(nombre, edad, esEstudiante, hobbies, persona);
